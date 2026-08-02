bef8da1215a7e1d6169bd0e4b926ed724021377a
Mansur <mansurvhai@gmail.com>
2026-08-02 07:46:06 +0600

    Complete logic building

--- CaloryCounter_Project/CalorieCounter/models.py
+++ CaloryCounter_Project/CalorieCounter/models.py
@@ -1,49 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+
 
 class User(AbstractUser):
-
-    def __str__(self):
+    
+   def __str__(self):
         return f"{self.username}"
-
-
+    
+    
 
 # Name, Age, Gender, Height, Weight
 
-class basicInfoModel(models.Model):
-
-    GENDER_TYPE = [
+class BasicInfoModel(models.Model):
+    
+    GENDER_TYPE =[
         ("Male", "Male"),
-        ("Female","FEmale")
+        ("Female", "Female")
     ]
-
-
-    user = models.OneToOneField(
-        User, 
-        on_delete=models.CASCADE, 
-        related_name='user_info', 
-        null=True
-        )
-    name = models.CharField(
-        max_length=100, 
-        null=True
-        )
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_info', null=True)
+    name = models.CharField(max_length=100, null=True)
     age = models.PositiveIntegerField(null=True)
-    gender = models.CharField(
-        choices=GENDER_TYPE, 
-        max_length=10, 
-        null=True
-        )
+    gender = models.CharField(choices=GENDER_TYPE, max_length=10, null=True)
     height = models.FloatField(null=True)
     weight = models.FloatField(null=True)
     bmr = models.FloatField(null=True)
-
-
-     
+    
+    
     def __str__(self):
         return f"{self.name} - {self.age}"
-
+    
+    
 # consumed calories (Item name, Calorie consumed)
 
 class ConsumedCalories(models.Model):
--- CaloryCounter_Project/CalorieCounter/views.py
+++ CaloryCounter_Project/CalorieCounter/views.py
@@ -1,3 +1,229 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from datetime import date
+from django.db.models import Sum,Count
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from CalorieCounter.models import *
+from CalorieCounter.forms import *
+
+
+def register_page(request):
+    
+    if request.method == "POST":
+        form_data = RegisterationForm(request.POST)
+        if form_data.is_valid():
+            form_data.save()
+            messages.success(request, 'Registeration successfully')
+            return redirect('login_page')
+    
+    
+    form_data = RegisterationForm()
+    
+    context ={
+        "form_data": form_data,
+        "form_title": "User Register Form",
+        "form_btn": "Register"
+    }
+    
+    return render(request, "master/base-form.html", context)
+
+
+
+def login_page(request):
+    
+    if request.method == "POST":
+       form_data = LoginForm(request, request.POST) 
+       if form_data.is_valid():
+           user = form_data.get_user()
+           login(request, user)
+           messages.success(request, 'Login successfully')
+           return redirect("dashboard_page")
+    
+    form_data = LoginForm()
+    context ={
+        "form_data": form_data,
+        "form_title": "User Login Form",
+        "form_btn": "Login"
+    }
+    
+    return render(request, "master/base-form.html", context)
+
+
+@login_required
+def logout_page(request):
+    logout(request)
+    messages.success(request, 'Logout successfully')
+    return redirect('login_page')
+@login_required
+
+def dashboard_page(request):
+    
+    try:
+        current_user = request.user
+        bmr = round(request.user.user_info.bmr, 2)
+    except:
+        bmr = 0
+    today = date.today()
+    today_consumed_date = ConsumedCalories.objects.filter(
+        consumed_by = current_user,
+        created_at = today
+    )
+    total_consumed_calories = today_consumed_date.aggregate(
+        total = Sum('calorie'),
+        total_count = Count('calorie')
+    )
+    
+    total_caloire = total_consumed_calories['total']
+    less_more = bmr - total_caloire
+    
+    if bmr > total_caloire:
+        suggestion = "Your calorie intake is below your daily requirement."
+    else:
+        suggestion = "Your calorie intake has exceeded your daily requirement."
+    
+    
+    
+    context ={
+        'required_calories': bmr,
+        'today_consumed_date':today_consumed_date,
+        'consumed_calories': total_caloire,
+        'total_count': total_consumed_calories['total_count'],
+        'less_more':less_more,
+        'suggestion': suggestion,
+        
+    }
+    
+    return render(request, 'dashboard.html', context)
+
+@login_required
+def profile_page(request):
+    
+    return render(request, 'profile.html')
+
+
+@login_required
+def update_profile(request):
+    
+    try:
+        current_user = request.user.user_info
+        
+    except:
+        current_user = None
+    
+    
+    
+    if request.method == 'POST':
+        form_data = ProfileUpdateForm(request.POST, instance=current_user)
+        if form_data.is_valid():
+            data = form_data.save(commit=False)
+            data.user = request.user
+            weight = data.weight
+            height = data.height
+            age = data.age
+            if data.gender == 'Male':
+                #BMR= 66.47+(13.75 x weight in kg) + (5.003 x height in cm) - (6.755 x age in years)
+                bmr_calculate = 66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age)
+            else:
+                #BMR=655.1+(9.563 x weight in kg)+(1.850 xheight in cm) - (4.676 x age in years)
+                bmr_calculate = 655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age)
+            data.bmr = bmr_calculate
+            data.save()
+            messages.success(request, 'Profile Update successfully')
+            return redirect('profile_page')
+    
+    form_data = ProfileUpdateForm(instance=current_user)
+    context = {
+        'form_data': form_data,
+        'form_title': "Update Profile Info",
+        'form_btn': "Update"
+    }
+    
+    return render(request, 'master/base-form.html', context)
+
+
+def consumed_calories_list(request):
+    
+    consumed_data = ConsumedCalories.objects.filter(consumed_by = request.user)
+    
+    context ={
+        "consumed_data": consumed_data,
+    }
+    
+    return render(request, 'calorie-list.html', context)
+
+
+def add_calorie(request):
+    
+    if request.method == "POST":
+        form_data = ConsumedCalorieForm(request.POST)
+        if form_data.is_valid():
+            data = form_data.save(commit=False)
+            data.consumed_by = request.user
+            data.save()
+            messages.success(request, 'successfully')
+            return redirect('consumed_calories_list')
+            
+    
+    
+    form_data = ConsumedCalorieForm()
+    
+    context = {
+        'form_data': form_data,
+        'form_title': "Add Calorie Info",
+        'form_btn': "Add Calorie",
+    }
+    
+    
+    return render(request, 'master/base-form.html', context)
+    
+    
+
+
+def Update_calorie(request, id):
+    
+    try:
+        data = ConsumedCalories.objects.get(id = id)
+    
+    except:
+        data = None
+    
+    if request.method == "POST":
+        form_data = ConsumedCalorieForm(request.POST,  instance=data)
+        if form_data.is_valid():
+            data = form_data.save(commit=False)
+            data.consumed_by = request.user
+            data.save()
+            messages.success(request, 'successfully')
+            return redirect('consumed_calories_list')
+            
+    
+    
+    form_data = ConsumedCalorieForm(instance=data)
+    
+    context = {
+        'form_data': form_data,
+        'form_title': "Update Calorie Info",
+        'form_btn': "Update Calorie",
+    }
+    
+    
+    return render(request, 'master/base-form.html', context)
+    
+    
+
+def delate_calorie(request, id):
+    
+    try:
+        data = ConsumedCalories.objects.get(id = id)
+    
+    except:
+        data = None
+        
+    data.delete()
+    messages.success(request, 'successfully')
+    return redirect('consumed_calories_list')
+    
